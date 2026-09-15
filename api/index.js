465ad083ac22c9ed0a172fdac25c057aa7215e34
dhakesh31411 <dhakesh31411@gmail.com>
2026-09-15 10:44:57 +0530

    Fix database diagnostics and registration API

--- api/index.js
+++ api/index.js
@@ -1,4 +1,9 @@
 const app = require('../backend/server');
 
-module.exports = app;
-
+module.exports = (req, res) => {
+  const targetUrl = req.headers['x-matched-path'] || req.headers['x-forwarded-uri'] || req.url;
+  if (targetUrl && typeof targetUrl === 'string' && targetUrl.startsWith('/api')) {
+    req.url = targetUrl;
+  }
+  return app(req, res);
+};
